db5e850f280512b8e2f2da382b2ce19501417392
Noah Kasmanoff <nkasmanoff@gmail.com>
2023-12-01 12:03:33 -0500

    add: updated docs

--- helio_tools/_src/editors/calibration.py
+++ helio_tools/_src/editors/calibration.py
@@ -1,3 +1,28 @@
+"""
+calibration.py
+
+This module provides functions for calibrating solar maps. It includes functionality to correct degradation in solar images and to fetch calibration tables.
+
+Functions:
+- get_auto_calibration_table() -> pd.DataFrame:
+    Fetches the auto calibration table from a remote server and caches it locally. If the table is already cached, it returns the cached version.
+
+- get_local_correction_table() -> astropy.table.Table:
+    Fetches the local correction table. If the table doesn't exist, it creates one.
+
+- correct_degregation(s_map: Map, method: Optional[str]="auto", **kwargs) -> Map:
+    Corrects the degradation in a solar map. The method of correction can be specified.
+
+Dependencies:
+- sunpy.map.Map: For handling solar data.
+- aiapy.calibrate: For correcting degradation in solar images.
+- pandas: For handling data frames.
+- astropy: For handling astronomical quantities and coordinates.
+- os, pathlib, urllib.request: For handling file paths and HTTP requests.
+
+Note: This module is designed to work with solar data and may not be applicable to other types of astronomical data.
+"""
+
 from typing import Optional
 import numpy as np
 from sunpy.map import Map
@@ -13,15 +38,36 @@ import astropy
 
 @lru_cache(maxsize=None)
 def get_auto_calibration_table():
+    """
+    Fetches the auto calibration table from a remote server and caches it locally. If the table is already cached, it returns the cached version.
+
+    Returns:
+    - pd.DataFrame: The auto calibration table.
+
+    Usage:
+    >>> table = get_auto_calibration_table()
+
+
+    """
     table_path = os.path.join(Path.home(), '.iti', 'sdo_autocal_table.csv')
     os.makedirs(os.path.join(Path.home(), '.iti'), exist_ok=True)
     if not os.path.exists(table_path):
-        request.urlretrieve('http://kanzelhohe.uni-graz.at/iti/sdo_autocal_table.csv', filename=table_path)
+        request.urlretrieve(
+            'http://kanzelhohe.uni-graz.at/iti/sdo_autocal_table.csv', filename=table_path)
     return pd.read_csv(table_path, parse_dates=['DATE'], index_col=0)
 
 
 @lru_cache(maxsize=None)
 def get_local_correction_table():
+    """
+    Fetches the local correction table. If the table doesn't exist, it creates one.
+
+    Returns:
+    - astropy.table.Table: The local correction table.
+
+    Usage:
+    >>> table = get_local_correction_table()
+    """
     path = os.path.join(Path.home(), 'aiapy', 'correction_table.dat')
     if os.path.exists(path):
         return get_correction_table(path)
@@ -32,19 +78,39 @@ def get_local_correction_table():
 
 
 def correct_degregation(
-        s_map: Map,
-        method: Optional[str]="auto",
-        **kwargs
+    s_map: Map,
+    method: Optional[str] = "auto",
+    **kwargs
 ) -> Map:
+    """
+    Corrects the degradation in a solar map. The method of correction can be specified.
+
+    Parameters:
+    - s_map (Map): The solar map to be corrected.
+    - method (Optional[str]): The method of correction. Can be "auto" or "aiapy". Defaults to "auto".
+    - **kwargs: Additional keyword arguments to be passed to the aiapy.calibrate.correct_degradation() function.
+
+    Returns:
+    - Map: The corrected solar map.
+
+    Usage:
+    >>> import helio_tools as ht
+    >>> s_map = ht.load_fits_to_map(filename)
+    >>> s_map = ht.correct_degradation(s_map, method="auto")
+
+    """
 
     if method == "auto":
         correction_table = get_auto_calibration_table()
-        index = correction_table["DATE"].sub(s_map.date.datetime).abs().idxmin()
+        index = correction_table["DATE"].sub(
+            s_map.date.datetime).abs().idxmin()
         num = s_map.meta["wavelnth"]
-        s_map = Map(s_map.data / correction_table.iloc[index][f"{int(num):04}"], s_map.meta)
+        s_map = Map(
+            s_map.data / correction_table.iloc[index][f"{int(num):04}"], s_map.meta)
     elif method == "aiapy":
         correction_table = get_local_correction_table()
-        s_map = aiapy_correct_degregation(smap=s_map, correction_table=correction_table, **kwargs)
+        s_map = aiapy_correct_degregation(
+            smap=s_map, correction_table=correction_table, **kwargs)
     elif method is None:
         pass
     else:
--- helio_tools/_src/editors/scale.py
+++ helio_tools/_src/editors/scale.py
@@ -1,3 +1,27 @@
+"""
+scale.py
+
+This module provides functions for normalizing and scaling solar maps. It includes functionality to normalize data using a provided function, normalize the radius of a solar map, and crop a solar map to a specified resolution.
+
+Functions:
+- normalize_fn(data: np.ndarray, fn: Callable) -> np.ndarray:
+    Normalizes the data array using the provided function. The function should take a numpy array as input and return a numpy array.
+
+- normalize_radius(s_map: Map, resolution: int, padding_factor: float = 0.1, crop: bool = True) -> Map:
+    Normalizes the radius of the solar map to a specified resolution. The padding factor and crop parameters are optional.
+
+- _crop_resolution(s_map: Map, resolution: int) -> Map:
+    Crops the solar map to the specified resolution. This is a helper function used within normalize_radius.
+
+Dependencies:
+- sunpy.map.Map: For handling solar data.
+- numpy: For numerical operations.
+- astropy: For handling astronomical quantities and coordinates.
+- warnings: For suppressing warnings during certain operations.
+
+Note: This module is designed to work with solar data and may not be applicable to other types of astronomical data.
+"""
+
 from typing import Callable
 from sunpy.map import Map
 import warnings
@@ -7,7 +31,25 @@ import numpy as np
 
 
 def normalize_fn(data: np.ndarray, fn: Callable) -> np.ndarray:
+    """Normalizes the data array using the provided function. 
+    The function should take a numpy array as input and return a numpy array.
+
+    Args:
+        data (np.ndarray): the data array to be normalized
+        fn (Callable): the function to be applied to the data array
+
+    Returns:
+        np.ndarray: the normalized data array
+
+    Usage:
+    >>> import helio_tools as ht
+    >>> import numpy as np
+    >>> data = np.random.rand(100, 100)
+    >>> fn = np.sin
+    >>> norm_data = ht.normalize_fn(data, fn)    
+    """
     return 2 * fn(data).data - 1
+
 
 def normalize_radius(
         s_map: Map,
@@ -15,13 +57,32 @@ def normalize_radius(
         padding_factor: float = 0.1,
         crop: bool = True,
 ) -> Map:
+    """ 
+    Normalizes the radius of the solar map to a specified resolution.
+
+    Args:
+        s_map (Map): the solar map to be normalized
+        resolution (int): the desired resolution of the output map
+        padding_factor (float, optional): the padding factor for the solar radius. Defaults to 0.1.
+        crop (bool, optional): whether to crop the map to the specified resolution. Defaults to True.
+
+    Returns:
+        Map: the normalized solar map
+
+    Usage:
+    >>> import helio_tools as ht
+    >>> import numpy as np
+    >>> s_map = ht.load_fits_to_map(filename)
+    >>> norm_map = ht.normalize_radius(s_map, 512)    
+    """
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         r_obs_pix = s_map.rsun_obs / s_map.scale[0]  # normalize solar radius
         r_obs_pix = (1 + padding_factor) * r_obs_pix
         scale_factor = resolution / (2 * r_obs_pix.value)
         s_map = Map(np.nan_to_num(s_map.data).astype(np.float32), s_map.meta)
-        s_map = s_map.rotate(recenter=True, scale=scale_factor, missing=0, order=4)
+        s_map = s_map.rotate(
+            recenter=True, scale=scale_factor, missing=0, order=4)
         if crop:
             s_map = _crop_resolution(s_map, resolution=resolution)
         s_map.meta['r_sun'] = s_map.rsun_obs.value / s_map.meta['cdelt1']
@@ -33,11 +94,30 @@ def _crop_resolution(
         s_map: Map,
         resolution: int
 ) -> Map:
+    """Crops the solar map to the specified resolution. 
+    This is a helper function used within normalize_radius.
+
+    Args:
+        s_map (Map): the solar map to be cropped
+        resolution (int): the desired resolution of the output map
+
+    Returns:
+        Map: the cropped solar map
+
+    Usage:
+    >>> import helio_tools as ht
+    >>> import numpy as np
+    >>> s_map = ht.load_fits_to_map(filename)
+    >>> s_map = ht.normalize_radius(s_map, 512)
+    >>> s_map = ht._crop_resolution(s_map, 512) # crop to 512x512 pixels
+
+    """
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         arcs_frame = (resolution / 2) * s_map.scale[0].value
         s_map = s_map.submap(
-            bottom_left=SkyCoord(-arcs_frame * u.arcsec, -arcs_frame * u.arcsec, frame=s_map.coordinate_frame),
+            bottom_left=SkyCoord(-arcs_frame * u.arcsec, -
+                                 arcs_frame * u.arcsec, frame=s_map.coordinate_frame),
             top_right=SkyCoord(arcs_frame * u.arcsec, arcs_frame * u.arcsec, frame=s_map.coordinate_frame))
         pad_x = s_map.data.shape[0] - resolution
         pad_y = s_map.data.shape[1] - resolution
