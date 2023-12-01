4377e0d7a27fe4c5f43989c985690fe63b7141e6
Noah Kasmanoff <nkasmanoff@gmail.com>
2023-12-01 12:05:34 -0500

    Normalize data in editors.py

--- helio_tools/_src/editors/editors.py
+++ helio_tools/_src/editors/editors.py
@@ -1,5 +1,4 @@
 from typing import Optional
-
 import numpy as np
 
 
@@ -9,6 +8,29 @@ def constrast_normalize(
     use_median: bool = False,
     normalization: Optional[float] = None,
 ):
+"""
+    Normalizes the data by subtracting the shift and dividing by the normalization. 
+    If shift is None, it is set to the median of the data. 
+    If normalization is None, it is set to the standard deviation of the data.
+
+    Args:
+
+    - data (np.ndarray): The data to be normalized.
+    - shift (float, optional): The value to be subtracted from the data. Defaults to None.
+
+    - use_median (bool, optional): If True, the median of the data is used as the shift. Otherwise, the mean is used. Defaults to False.
+    - normalization (float, optional): The value to be divided by. Defaults to None.
+
+    Returns:
+
+    - np.ndarray: The normalized data.
+
+    Usage:
+
+    >>> import helio_tools as ht
+    >>> data = ht.load_fits_to_ndarray(filename)
+    >>> data = ht.contrast_normalize(data)
+"""
     if shift is None:
         shift = np.nanmedian(data) if use_median else np.nanmean(data)
 
