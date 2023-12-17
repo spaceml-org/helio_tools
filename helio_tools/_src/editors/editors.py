fac7a235a1debdb3ab446dcd9b454409ec221f81
Noah Kasmanoff <nkasmanoff@gmail.com>
2023-12-17 10:36:09 -0500

    add: unchecked editors

--- helio_tools/_src/editors/editors.py
+++ helio_tools/_src/editors/editors.py
@@ -5,6 +5,8 @@ import random
 from random import randint
 from scipy import ndimage
 from sunpy.map import Map
+from astropy import units as u
+from astropy.coordinates import SkyCoord
 
 
 class Editor(ABC):
@@ -199,3 +201,150 @@ class LoadMapEditor(Editor):
             s_map = Map(data)
             s_map.meta['timesys'] = 'tai'  # fix leap seconds
             return s_map, {'path': data}
+
+
+class NormalizeExposureEditor(Editor):
+    """
+    Normalizes the exposure time of a sunpy map to a target value.
+
+    Parameters
+    ----------
+    target : `astropy.units.Quantity`
+        The target exposure time.
+
+    Usage:
+
+    >>> import helio_tools as ht
+    >>> editor = ht.NormalizeExposureEditor(target=1 * u.s)
+    >>> s_map = editor(s_map)
+
+
+    """
+
+    def __init__(self, target=1 * u.s):
+        self.target = target
+        super().__init__()
+
+    def call(self, s_map, **kwargs):
+        warnings.simplefilter("ignore")
+        data = s_map.data
+        data = data / \
+            s_map.exposure_time.to(u.s).value * self.target.to(u.s).value
+        return Map(data.astype(np.float32), s_map.meta)
+
+
+class ScaleEditor(Editor):
+    """
+
+    Scales a sunpy map to a target arcseconds per pixel value.
+
+    Parameters
+    ----------
+    arcspp : `float`
+        The target arcseconds per pixel value.
+
+    Usage:
+
+    >>> import helio_tools as ht
+    >>> editor = ht.ScaleEditor(arcspp=0.6)
+    >>> s_map = editor(s_map)
+
+    """
+
+    def __init__(self, arcspp):
+        self.arcspp = arcspp
+        super(ScaleEditor, self).__init__()
+
+    def call(self, s_map, **kwargs):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")  # ignore warnings
+            scale_factor = s_map.scale[0].value / self.arcspp
+            new_dimensions = [int(s_map.data.shape[1] * scale_factor),
+                              int(s_map.data.shape[0] * scale_factor)] * u.pixel
+            s_map = s_map.resample(new_dimensions)
+
+            return Map(s_map.data.astype(np.float32), s_map.meta)
+
+
+class CropEditor(Editor):
+    """
+
+    Crops a sunpy map to specified dimensions.
+
+    Parameters
+    ----------
+    start_x : `int`
+        The starting x coordinate of the crop.
+    end_x : `int`
+        The ending x coordinate of the crop.
+    start_y : `int`
+        The starting y coordinate of the crop.
+    end_y : `int`
+        The ending y coordinate of the crop.
+
+    Usage:
+
+    >>> import helio_tools as ht
+    >>> editor = ht.CropEditor(start_x=0, end_x=512, start_y=0, end_y=512)
+    >>> s_map = editor(s_map)
+
+    """
+
+    def __init__(self, start_x, end_x, start_y, end_y):
+        self.start_x = start_x
+        self.end_x = end_x
+        self.start_y = start_y
+        self.end_y = end_y
+
+    def call(self, data, **kwargs):
+
+        crop = data[self.start_x: self.end_x, self.start_y:self.end_y]
+        return crop
+
+
+class ShiftMeanEditor(Editor):
+    """
+
+    Normalizes the data, by setting the mean to 0, and clipping the data to [-1, 1].
+
+    Usage:
+
+    >>> import helio_tools as ht
+    >>> editor = ht.ShiftMeanEditor()
+    >>> data = ht.load_fits_to_ndarray(filename)
+    >>> data = editor(data)
+
+    """
+
+    def call(self, data, **kwargs):
+        mean = np.mean(data)
+        data = (data - mean)
+        data = np.clip(data, -1, 1)
+        return data
+
+
+class NormalizeRadiusEditor(Editor):
+    def __init__(self, resolution, padding_factor=0.1, crop=True, **kwargs):
+        self.padding_factor = padding_factor
+        self.resolution = resolution
+        self.crop = crop
+        super(NormalizeRadiusEditor, self).__init__(**kwargs)
+
+    def call(self, s_map, **kwargs):
+        warnings.simplefilter("ignore")  # ignore warnings
+        r_obs_pix = s_map.rsun_obs / s_map.scale[0]  # normalize solar radius
+        r_obs_pix = (1 + self.padding_factor) * r_obs_pix
+        scale_factor = self.resolution / (2 * r_obs_pix.value)
+        s_map = Map(np.nan_to_num(s_map.data).astype(np.float32), s_map.meta)
+        s_map = s_map.rotate(
+            recenter=True, scale=scale_factor, missing=0, order=4)
+        if self.crop:
+            arcs_frame = (self.resolution / 2) * s_map.scale[0].value
+            s_map = s_map.submap(bottom_left=SkyCoord(-arcs_frame * u.arcsec, -arcs_frame * u.arcsec, frame=s_map.coordinate_frame),
+                                 top_right=SkyCoord(arcs_frame * u.arcsec, arcs_frame * u.arcsec, frame=s_map.coordinate_frame))
+            pad_x = s_map.data.shape[0] - self.resolution
+            pad_y = s_map.data.shape[1] - self.resolution
+            s_map = s_map.submap(bottom_left=[pad_x // 2, pad_y // 2] * u.pix,
+                                 top_right=[pad_x // 2 + self.resolution - 1, pad_y // 2 + self.resolution - 1] * u.pix)
+        s_map.meta['r_sun'] = s_map.rsun_obs.value / s_map.meta['cdelt1']
+        return s_map
